ed6f62add5a430a077fa45a5918340350148c798
Edxael <zerphyr.r@gmail.com>
2017-12-13 23:38:26 -0700

    1st Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class IndexComp extends Component {
+  render() {
+    return(
+      <div>Hello</div>
+    )
+  }
+}
+
+ReactDOM.render( <IndexComp/>, document.getElementById('root') )
